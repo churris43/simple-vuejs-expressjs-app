3d7226995f69f96cd346c7eb6a2a92e64ee41a74
churris43 <churris43@hotmail.com>
2025-12-24 11:17:34 +1100

    Create the test db table as part of building the project

---
+++ db/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS test(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    create_time DATETIME COMMENT 'Create Time',
+    name VARCHAR(255) NOT NULL
+) COMMENT '';
