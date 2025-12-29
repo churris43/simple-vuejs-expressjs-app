18fe33148948ce7c7203fa908c5987a477af368a
churris43 <churris43@hotmail.com>
2025-12-29 11:57:45 +1100

    Highlight the active menu item

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router';
+
+const isActiveLink = (routePath: string): boolean =>   {
+    const route = useRoute();
+    return route.path === routePath;
+}
 </script>
 
 <template>
     <ul>
-        <li><RouterLink to="/">Home</RouterLink></li>
-        <li><RouterLink to="/applications">Applications</RouterLink></li>
+        <li><RouterLink
+            :class="[isActiveLink('/') ? 'bg-amber-100' : '']"
+            to="/">Home</RouterLink>
+        </li>
+        <li><RouterLink
+            :class="[isActiveLink('/applications') ? 'bg-amber-100' : '']"
+             to="/applications">Applications</RouterLink>
+        </li>
     </ul>
 </template>
