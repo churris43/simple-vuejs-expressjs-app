7b446fbaa9db4234ad436332f2e3bd1500d4657a
churris43 <churris43@hotmail.com>
2025-12-29 14:12:59 +1100

    Not found page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue'; 
 import ApplicationsView from '../views/ApplicationsView.vue';
+import NewApplicationView from '../views/NewApplicationView.vue';
+import NotFound from '../views/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +17,16 @@ const router = createRouter({
             name: 'applications',
             component: ApplicationsView,
         },
+        {
+            path: '/new',
+            name: 'new',
+            component: NewApplicationView,
+        },
+        {
+            path: '/:catchAll(.*)',
+            name: "not-found",
+            component: NotFound,
+        }
     ],
 });
 
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <span class="pi pi-check"></span>
+    <RouterLink to="/">Go to HomePage</RouterLink>
+</template>
