cce158d72e9ce18c0f71968e1094f146b172972f
churris43 <churris43@hotmail.com>
2025-12-29 14:13:55 +1100

    Code clean up

--- frontend/src/components/HomeCard.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-    <p> this is the content of the home card</p>
-</template>
--- frontend/src/views/ApplicationsView.vue
+++ frontend/src/views/ApplicationsView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
     import Hero from '../components/Hero.vue';
-    import HomeCard from '../components/HomeCard.vue';
     import ApplicationListings from '../components/ApplicationListings.vue';
 </script>
 
 <template>
     <Hero title="List of Applications" subtitle="Here is the list of Applications"/>
-    <HomeCard />
     <ApplicationListings :limit="4"/>
 </template>
